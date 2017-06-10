280099674b017f44e64049a7c195f05a2ca8e2bd
Bill Rainey <billrainey@lucidmotors.com>
2017-06-09 23:36:28 -0700

    Added /tasks route to ntask app.

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ import express from "express"
 const PORT = 3000;
 const app = express();
 
+app.set("json spaces", 4);
+
 app.get("/", (req, res) => res.json({status: "NTask API"}));
+
+app.get("/tasks", (req, res) => {
+	res.json({
+		tasks: [
+			{title: "Buy some shoes"},
+			{title: "Fix notebook"}
+		]
+	});
+});
 
 app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
