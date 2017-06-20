d52dfb5982139ab085fe06d6105d485efa235fb1
Bill Rainey <billrainey@lucidmotors.com>
2017-06-20 01:42:24 -0700

    Initial pass at implementing CRUD functions for tasks.

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,56 @@
 module.exports = app => {
 	const Tasks = app.db.models.Tasks;
-	app.get("/tasks", (req, res) => {
-		Tasks.findAll({title: "foo", "req": req}).then(tasks => {
-			res.json({tasks: tasks});
+
+	app.route("/tasks")
+		.all((req, res) => {
+			delete req.body.id;
+			next();
+		})
+		.get((req, res) => {
+			Tasks.findAll({})
+				.then(result => res.json(result))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		})
+		.post((req, res) => {
+			Tasks,create(req,body)
+				.then(result => res.json(result))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
 		});
-	});
+
+	app.route("/tasks/:id")
+		.all((req, res) => {
+			delete req.body.id;
+			next();
+		})
+		.get((req, res) => {
+			Tasks.findOne({where: req.params})
+				.then(result => {
+					if (result) {
+						res.json(result);
+					} else {
+						res.sendStatus(404);
+					}
+				})
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		})
+		.put((req, res) => {
+			Tasks.update(req.body, {where: req.params})
+				.then(result => res.sendStatus(204))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		})
+		.delete((req, res) => {
+			Tasks.destroy({where: req.params})
+				.then(result => res.sentStatus(204))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		});
 };
