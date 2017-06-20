bff100dbe5dc8ed8f04a3ca120273bd5c07b86e9
Bill Rainey <billrainey@lucidmotors.com>
2017-06-20 02:23:08 -0700

    Changes for the conclusion of chapter 6.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,5 +1,12 @@
+import bodyParser from "body-parser";
+
 module.exports = app => {
-	const PORT = process.env.PORT || 5000;
-	app.set("port", PORT);
-	app.set("json s[aces", 4);
+  const PORT = process.env.PORT || 5000;
+  app.set("port", PORT);
+  app.set("json s[aces", 4);
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
 };
