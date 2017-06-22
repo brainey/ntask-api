01bd5ecdd8f683d1891d0503a2448d85c97b6d75
Bill Rainey <billrainey@lucidmotors.com>
2017-06-22 11:02:04 -0700

    Changes for first test of chapter 8.

---
+++ test/routes/index.js
@@ -0,0 +1,15 @@
+import chai from "chai";
+expect = chai.expect;
+describe("Routes: Index", () => {
+  describe("GET /", () => {
+    it("returns the API status", done => {
+      request.get("/")
+        .expect(200)
+        .end((err, res) => {
+          const expected = { status: "NTask API"};
+          expect(res.body).to.eql(expected);
+          done(err);
+        });
+    });
+  });
+});
