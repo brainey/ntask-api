bc329692ab998b9803d7cfcc1bffd40b763a7e50
Bill Rainey <billrainey@lucidmotors.com>
2017-06-26 04:15:48 -0700

    Added API documentation.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,13 +1,15 @@
 import bodyParser from "body-parser";
+import express from "express";
 
 module.exports = app => {
   const PORT = process.env.PORT || 3000;
   app.set("port", PORT);
-  app.set("json s[aces", 4);
+  app.set("json spaces", 4);
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
     delete req.body.id;
     next();
   });
+  app.use(express.static("public"));
 };
