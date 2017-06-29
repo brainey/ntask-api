8c6e3c2d6b3c31c285c00abcadd513b82d9aa7d9
Bill Rainey <billrainey@lucidmotors.com>
2017-06-29 05:42:22 -0700

    Added cert for HTTPS service and made use of HTTPS conditional on environment variable USE_HTTP.

--- libs/boot.js
+++ libs/boot.js
@@ -1,9 +1,23 @@
+import https from "https";
+import fs from "fs";
+
 module.exports = app => {
-	if (process.env.NODE_ENV !== "test") {
-		app.db.sequelize.sync().done(() => {
-			app.listen(app.get("port"), () => {
-				console.log(`NTask API - Port ${app.get("port")}`);
-			});
-		});
-	}
+  if (process.env.NODE_ENV !== "test") {
+    const credentials = {
+      key: fs.readFileSync("ntask.key", "utf8"),
+      cert: fs.readFileSync("ntask.cert", "utf8")
+    }
+    app.db.sequelize.sync().done(() => {
+      if (process.env.USE_HTTP) {
+        app.listen(app.get("port"), () => {
+          console.log(`NTask API - Port ${app.get("port")}`);
+        });
+      } else {
+        https.createServer(credentials, app)
+          .listen(app.get("port"), () => {
+            console.log(`NTask API - Port ${app.get("port")}`);
+        });
+      }
+    });
+  }
 };
