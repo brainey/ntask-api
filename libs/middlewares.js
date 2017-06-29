438f3da8c1755e6f146774d68fddd9ba968bc00a
Bill Rainey <billrainey@lucidmotors.com>
2017-06-29 10:55:01 -0700

    Added some features like compression and security headers that I accidentally skipped.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -2,6 +2,8 @@ import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import compression from "compression";
+import helmet from "helmet";
 import logger from "./logger.js";
 
 module.exports = app => {
@@ -16,11 +18,13 @@ module.exports = app => {
       }
     }
   }));
+  app.use(helmet());
   app.use(cors({
     origin: ["http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowHeaders: ["Content-Type", "Authorization"]
   }));
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
